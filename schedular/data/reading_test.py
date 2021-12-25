ceb0e355554461403327a6915c942f2b93e4eb4b
cchave17 <carloschavezdeveloper@gmail.com>
2021-12-25 09:25:38 -0600

    added stuff

---
+++ schedular/data/reading_test.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+ExcelRead = pd.read_excel("D:\Personal-Projects\mock-schedular\schedular\data\studyplanexcel.xlsx")
+
+for (columnName, columnData) in ExcelRead.iteritems():
+#    for data in columnData.values:
+    ExcelRead.dropna(inplace=True)
+    print('Column Name : ', columnName)
+    print('Column Contents : ', columnData.values)
